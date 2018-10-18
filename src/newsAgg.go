b7d4ca5894ee4abe3963e3b6347787058a65ec3a
Brandon Autrey <brandon.c.autrey@gmail.com>
2018-10-18 07:16:11 -0400

    Added concurrency to go file; reformat

--- src/newsAgg.go
+++ src/newsAgg.go
@@ -1,51 +1,68 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"net/http"
 	"html/template"
 	"io/ioutil"
-	"encoding/xml"
+	"net/http"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type SitemapIndex struct {
 	Locations []string `xml:"sitemap>loc"`
 }
 
 type News struct {
-	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>keywords"`
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
 }
 
 type NewsMap struct {
-	Keyword string
+	Keyword  string
 	Location string
 }
 
 type NewsAggPage struct {
 	Title string
-	News map[string]NewsMap
+	News  map[string]NewsMap
+}
+
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
+	var n News
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+	c <- n
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
 	var s SitemapIndex
-	var n News
-	
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap)
-
+	resp.Body.Close()
+	queue := make(chan News, 30)
 	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
-		
-		for idx, _ := range n.Titles{
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+
+	wg.Wait()
+	close(queue)
+
+	for elem := range queue {
+		for idx, _ := range elem.Keywords {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
-	} 
+	}
 
 	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
 	t, _ := template.ParseFiles("newsaggtemplate.html")
